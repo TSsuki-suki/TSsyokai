13fc9d120016a9091c3216bbae1737e0f359d897
大葉 <ifyuser0719@gmail.com>
2026-08-14 11:41:27 +0900

    Update works.js

--- data/works.js
+++ data/works.js
@@ -52,5 +52,56 @@ const works = [
 
         // 画像
         image: ""
-    }
+    },
+    
+    {
+    id: 2,
+
+    title: "お兄ちゃんはおしまい！",
+
+    author: "ねことうふ",
+
+    site: "pixivなど多サイト",
+
+    media: "漫画",
+
+    derivative: "オリジナル",
+
+    originalWork: "",
+
+    status: "連載中",
+
+    ageRating: "全年齢",
+
+    tsType: [
+        "男→女",
+        "可逆TS",
+        "朝おん"
+    ],
+
+    genres: [
+        "コメディ",
+        "日常"
+    ],
+
+    tsMain: 8,
+
+    recommendation: 10,
+
+    catchphrase: "TSアニメといえばこれ！",
+
+    summary:
+        "自宅警備員としての日々を過ごしていた主人公、みはり。しかし妹に仕込まれた薬を飲んで気づいたら女の子になっちゃった！",
+
+    points:
+        "お決まり展開が多く、思考が女になっていくタイプが好きなTSスキーには喜びの作品",
+
+    comment:
+        "すごくおすすめ。ただTS初心者にはあまり推奨しないものでもある。TSスキーの諸君なら分かるはず。",
+
+    url:
+        "https://www.pixiv.net/user/159912/series/12",
+
+    image: ""
+}
 ];
